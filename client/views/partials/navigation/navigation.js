9db2fbfd889a629602f0eed36d8d9676624d99b0
Elliott Hughes-Taylor <ehughestaylor@Elliotts-MacBook-Pro.local>
2015-12-17 12:05:54 -0500

    Added styling for mobile on landing page as well as functionality for user search in the navigation

--- client/views/partials/navigation/navigation.js
+++ client/views/partials/navigation/navigation.js
@@ -1,17 +1,18 @@
 
-// Template.navigation.rendered=function(){
-//   Meteor.typeahead.inject();
-//   $('.typeahead').bind('typeahead:select', function(ev, suggestion) {
-//     console.log('Selection: ', suggestion.value);
-//     Router.go('profile', { username: suggestion.value });
-//   });
-// };
+Template.navigation.rendered=function(){
+  Meteor.typeahead.inject();
+  $('.typeahead').bind('typeahead:select', function(ev, suggestion) {
+    console.log('Selection: ', suggestion.value);
+    Router.go('member', { username: suggestion.value });
+  });
+};
 
-// Template.navigation.helpers({
-//   usernames: function(){
-//     return Meteor.users.find().fetch().map(function(user){
-//       return user.username;
-//       return User.services.facebook.name;
-//     });
-//   }
-// });
+Template.navigation.helpers({
+  usernames: function(){
+    return Meteor.users.find().fetch().map(function(user){
+      return user.username;
+      return user.emails[0].address;
+      return user.createdAt;
+    });
+  }
+});
--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,9 @@
-Meteor.publish("userData", function () {
-    return Meteor.users.find({_id: this.userId},
-        {fields: {'username': 1, 'email': 1}});
+// Meteor.publish("userData", function () {
+//     return Meteor.users.find({_id: this.userId},
+//         {fields: {'username': 1, 'email': 1}});
+// });
+Meteor.publish('usersData', function () {
+  return Meteor.users.find({});
 });
 Meteor.publish('products', function(){
   return Products.find();
